3a05294d6cece7dab0d6fef1b221b03eace9b5ab
vahid <vahid.d.w@gmail.com>
2021-10-27 21:22:03 +0330

    create SavingAccount class extends Account

---
+++ src/com/vahidsaghlatoon/bankservice/SavingAccount.java
@@ -0,0 +1,7 @@
+package com.vahidsaghlatoon.bankservice;
+
+public class SavingAccount extends Account{
+    public SavingAccount(int number, double balance) {
+        super(number, balance);
+    }
+}
