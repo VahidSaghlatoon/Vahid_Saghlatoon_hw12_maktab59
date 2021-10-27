f66868735f0dffa8f744e99d60114ec360c5f824
vahid <vahid.d.w@gmail.com>
2021-10-27 20:53:10 +0330

    create bank class

---
+++ src/com/vahidsaghlatoon/bankservice/Bank.java
@@ -0,0 +1,30 @@
+package com.vahidsaghlatoon.bankservice;
+
+public class Bank {
+    private  int code ;
+    private String address ;
+
+    public Bank(int code, String address) {
+        this.code = code;
+        this.address = address;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void manages(){}
+    public void maintains(){}
+}
